045a296776b0f0fed3d15a770140c66fabd5db3c
Lucas Felix Santos <started.lucasfelixsantos@gmail.com>
2025-05-01 12:46:42 -0300

    refactor: reestruturação geral do projeto com inclusão do README

--- src/app/AppMain.java
+++ src/app/AppMain.java
@@ -1,10 +1,8 @@
 package app;
-import view.MenuInicial;
 
 public class AppMain {
 
 	public static void main(String[] args) {
-		MenuInicial novoMenu = new MenuInicial();
-		novoMenu.inicio();
+		
 	}
 }
---
+++ src/view/InterfaceInicial.java
@@ -0,0 +1,8 @@
+package view;
+
+public class InterfaceInicial {
+
+	public static void main(String[] args) {
+
+	}
+}
